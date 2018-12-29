dbb2149a3aab7f5002680162bc28c4b2541482f1
Mounika <mounikakunchanapalli@achiever-124.local>
2018-12-29 15:26:01 +0530

    hi

---
+++ hinew/hinew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hinew
+//
+//  Created by Mounika on 12/29/18.
+//  Copyright © 2018 Mounika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
